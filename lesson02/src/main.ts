31603c1c40251a1dd2f08f8d65dda7905dd13be8
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-25 12:44:57 +0500

    Lesson02. Basic Types.

---
+++ lesson02/src/main.ts
@@ -0,0 +1,20 @@
+let myName: string = 'Anatoliy'
+let meaningOfLife: number
+let isLoading: boolean
+let album: any
+
+myName = 'Tolik'
+
+meaningOfLife = 42
+isLoading = true
+
+album = 1984
+
+const sum = (a: number, b: number) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
